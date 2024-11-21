55d509664842ff1e76b3089e563b5451fe46dc2b
Nikita Muzikants <nikita.muzikants@gmail.com>
2024-11-21 20:59:48 +0200

    [feature/TWP-1337]: Learn Variables

---
+++ src/main/java/lv/acodemy/objects/Car.java
@@ -0,0 +1,43 @@
+package lv.acodemy.objects;
+
+public class Car {
+    // Fields
+    private String color;
+    private String model;
+
+    public Car(String color, String model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    public Car() {
+    }
+
+    public void start() {
+        System.out.println("Car started");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
